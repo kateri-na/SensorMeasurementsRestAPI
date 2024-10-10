d5bc8aa0fd50058469df83796adf25ba9f7c6ad7
katerina <katerina_panina1@mail.ru>
2024-10-10 21:14:41 +0300

    add constructor and fix validation

--- src/main/java/ru/Panina/SensorMeasurementsRestAPI/models/Sensor.java
+++ src/main/java/ru/Panina/SensorMeasurementsRestAPI/models/Sensor.java
@@ -5,7 +5,9 @@ import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,13 +19,14 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Sensor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
 
-    @NotNull
+    @NotEmpty(message = "Имя сенсора обязательно")
     @Min(value = 3, message = "Имя сенсора должно содержать как минимум 3 символа")
     @Max(value = 30, message = "Имя сенсора может содержать максимум 30 символов")
     @Column(name = "name")

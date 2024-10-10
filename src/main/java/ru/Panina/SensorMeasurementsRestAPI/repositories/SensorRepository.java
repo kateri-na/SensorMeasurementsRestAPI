3df9ce8ee9ad4cdca8b39abe48f102cc298412ae
katerina <katerina_panina1@mail.ru>
2024-10-10 21:16:19 +0300

    add required method

--- src/main/java/ru/Panina/SensorMeasurementsRestAPI/repositories/SensorRepository.java
+++ src/main/java/ru/Panina/SensorMeasurementsRestAPI/repositories/SensorRepository.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Repository;
 import ru.Panina.SensorMeasurementsRestAPI.controllers.SensorController;
 import ru.Panina.SensorMeasurementsRestAPI.models.Sensor;
 
+import java.util.Optional;
+
 @Repository
 public interface SensorRepository extends JpaRepository<Sensor, Integer> {
+    public Sensor findByName(String name);
 }

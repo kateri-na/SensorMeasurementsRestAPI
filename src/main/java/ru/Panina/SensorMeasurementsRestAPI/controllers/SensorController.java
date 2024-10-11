465abddb2bd8985c7904073d91af94bb71e5d236
katerina <katerina_panina1@mail.ru>
2024-10-11 22:24:26 +0300

    add dto converter

--- src/main/java/ru/Panina/SensorMeasurementsRestAPI/controllers/SensorController.java
+++ src/main/java/ru/Panina/SensorMeasurementsRestAPI/controllers/SensorController.java
@@ -6,11 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import ru.Panina.SensorMeasurementsRestAPI.dto.SensorDTO;
 import ru.Panina.SensorMeasurementsRestAPI.models.Sensor;
 import ru.Panina.SensorMeasurementsRestAPI.services.SensorService;
-import ru.Panina.SensorMeasurementsRestAPI.util.SensorAlreadyExistsException;
-import ru.Panina.SensorMeasurementsRestAPI.util.SensorErrorResponse;
+import ru.Panina.SensorMeasurementsRestAPI.util.SensorNotCreatedException;
+import ru.Panina.SensorMeasurementsRestAPI.util.ErrorResponse;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/sensors")
@@ -20,19 +24,31 @@ public class SensorController {
     private final SensorService sensorService;
 
     @PostMapping("/registration")
-    public ResponseEntity<HttpStatus> create(@RequestBody @Valid Sensor sensor,
+    public ResponseEntity<HttpStatus> create(@RequestBody @Valid SensorDTO sensorDTO,
                                              BindingResult bindingResult){
         if(bindingResult.hasErrors()){
-
+            StringBuilder errorMessage = new StringBuilder();
+            List<FieldError> errors = bindingResult.getFieldErrors();
+            for(FieldError error:errors){
+                errorMessage.append(error.getField()).append("-").
+                        append(error.getDefaultMessage()).append(";");
+            }
+            throw new SensorNotCreatedException(errorMessage.toString());
         }
-        sensorService.save(sensor);
+        sensorService.save(convertToSensor(sensorDTO));
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
     @ExceptionHandler
-    private ResponseEntity<SensorErrorResponse> handleException(SensorAlreadyExistsException e){
-        SensorErrorResponse response = new SensorErrorResponse("В системе уже сущесвует датчик с таким именем",
+    private ResponseEntity<ErrorResponse> handleException(SensorNotCreatedException e){
+        ErrorResponse response = new ErrorResponse(e.getMessage(),
                 System.currentTimeMillis());
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND); //404 status
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST); //404 status
+    }
+
+    private Sensor convertToSensor(SensorDTO sensorDTO){
+        Sensor sensor = new Sensor();
+        sensor.setName(sensorDTO.getName());
+        return sensor;
     }
 }

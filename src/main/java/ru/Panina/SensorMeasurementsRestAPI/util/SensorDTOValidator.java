1c9050cc549699af15061f1bfd9d911a7a76adf9
katerina <katerina_panina1@mail.ru>
2024-10-16 18:26:32 +0300

    change sensor validator to sensor dto validator

--- src/main/java/ru/Panina/SensorMeasurementsRestAPI/util/SensorDTOValidator.java
+++ src/main/java/ru/Panina/SensorMeasurementsRestAPI/util/SensorDTOValidator.java
@@ -6,22 +6,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
+import ru.Panina.SensorMeasurementsRestAPI.dto.SensorDTO;
 import ru.Panina.SensorMeasurementsRestAPI.models.Sensor;
 import ru.Panina.SensorMeasurementsRestAPI.services.SensorService;
 
 @Component
 @AllArgsConstructor
-public class SensorValidator implements Validator {
+public class SensorDTOValidator implements Validator {
     @Autowired
     private final SensorService sensorService;
     @Override
     public boolean supports(Class<?> clazz) {
-        return Sensor.class.equals(clazz);
+        return SensorDTO.class.equals(clazz);
     }
 
     @Override
     public void validate(Object target, Errors errors) {
-        Sensor sensor = (Sensor) target;
+        SensorDTO sensorDTO = (SensorDTO) target;
+        Sensor sensor = sensorService.convertToSensor(sensorDTO);
         if(sensorService.findSensor(sensor.getName())!=null){
             errors.rejectValue("name", "",
                     "Сенсор с таким названием уже существует в системе");
